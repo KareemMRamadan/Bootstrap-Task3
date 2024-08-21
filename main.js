6eda3f76c5d3ad26b5e19f077463257bbd446f4d
“kareem <kareemramadan260@gmail.com>
2024-08-21 15:25:21 +0300

    onclick button

---
+++ main.js
@@ -0,0 +1,31 @@
+function appear() {
+  let content = document.getElementById("content");
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
+
+function Theme() {
+  let img = document.getElementById("img");
+  if (
+    img.src.includes(
+      "https://img.icons8.com/?size=100&id=45475&format=png&color=000000"
+    )
+  ) {
+    img.src =
+      "https://img.icons8.com/?size=100&id=SyBDr18WdUcD&format=png&color=000000";
+  } else {
+    img.src =
+      "https://img.icons8.com/?size=100&id=45475&format=png&color=000000";
+  }
+  let body = document.body;
+  if (body.style.backgroundColor === "black") {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  } else {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  }
+}
